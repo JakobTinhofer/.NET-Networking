5e3a6407cc7aa11dcf132a59f387afcded55780b
TINHOFER Jakob <jak.tinhofer@billrothgymnasium.at>
2023-08-29 00:19:46 +0200

    added count debug method

--- LightBlueFox.Connect/Util/DebugUtils.cs
+++ LightBlueFox.Connect/Util/DebugUtils.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace LightBlueFox.Connect.Util
 {
-    internal static class DebugUtils
+    public static class DebugUtils
     {
         private static DateTime _startTime = DateTime.UtcNow;
         private static Stopwatch _stopWatch = Stopwatch.StartNew();
@@ -37,17 +37,33 @@ namespace LightBlueFox.Connect.Util
         private static ObjectIDGenerator idGen = new ObjectIDGenerator();
         public static void Log(object tObj, string message, params object?[] args)
         {
-            var ddd = false;
+            writeTolog(tObj, message, args);
+        }
+
+        private static void writeTolog(object t, string message, params object?[] args)
+        {
             Debug.WriteLine("[ {0} @ {1} | i: {2} | T: {3}] {4}",
                 UtcNow.ToString("ss:FFFFFFF"),
-                (new System.Diagnostics.StackTrace())?.GetFrame(1)?.GetMethod()?.Name,
-                idGen.GetId(tObj, out ddd),
+                (new System.Diagnostics.StackTrace())?.GetFrame(2)?.GetMethod()?.Name,
+                idGen.GetId(t, out _),
                 Environment.CurrentManagedThreadId,
                 string.Format(message, args));
         }
 
-        
-
+        private static Dictionary<int, int> counts = new Dictionary<int, int>();
+        static Mutex logMutex = new Mutex();
+        public static void LogCount(object tObj, string tag, int? expected = null, string? msg = null) {
+            logMutex.WaitOne();
+            int objId = (int)idGen.GetId(tObj, out _);
+            int key = HashCode.Combine(objId, tag);
+            if(!counts.ContainsKey(key))
+            {
+                counts.Add(key, 0);
+            }
+            counts[key]++;
+            logMutex.ReleaseMutex();
+            writeTolog(tObj, " COUNT \"{0}\" (object {1}): {2}{3}.{4}", tag, objId, counts[key], expected == null ? "" : " of " + expected, msg ?? "");
+        }
 
         
     }
