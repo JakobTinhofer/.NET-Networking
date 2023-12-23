e7b55489cc87f9a5ebe99ed2034d82b03623b3ca
TINHOFER Jakob <jak.tinhofer@billrothgymnasium.at>
2023-12-23 19:13:52 +0100

    added very rudamentary tests

---
+++ Tests.LightBlueFox.Connect/CustomProtocol/ProtocolValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using LightBlueFox.Connect.CustomProtocol.Protocol;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.LightBlueFox.Connect.CustomProtocol
+{
+    [TestClass]
+    public class ProtocolValidatorTests
+    {
+        [TestMethod]
+        public void TestBaseValidation()
+        {
+            ProtocolDefinition def1 = new ProtocolDefinition(new(), typeof(TestMessages.TestMessage1), typeof(TestMessages.TestMessage2));
+
+            ProtocolDefinition def2 = new ProtocolDefinition(new(), typeof(TestMessages.TestMessage2), typeof(TestMessages.TestMessage1));
+
+            Assert.IsTrue(def1.Validator.ValidateChallenge(def2.Validator.GetChallengeBytes()));
+            Assert.IsTrue(def2.Validator.ValidateChallenge(def1.Validator.GetChallengeBytes()));
+            Assert.IsTrue(def1.Validator.ValidateAnswer(def2.Validator.GetAnswerBytes()));
+            Assert.IsTrue(def2.Validator.ValidateAnswer(def1.Validator.GetAnswerBytes()));
+        }
+
+        [TestMethod]
+        public void TestInvalidValidation()
+        {
+            ProtocolDefinition def1 = new ProtocolDefinition(new(), typeof(TestMessages.TestMessage1), typeof(TestMessages.TestMessage2));
+
+            ProtocolDefinition def2 = new ProtocolDefinition(new(), typeof(TestMessages.TestMessage2));
+
+            Assert.IsTrue(!def1.Validator.ValidateChallenge(def2.Validator.GetChallengeBytes()));
+            Assert.IsTrue(!def2.Validator.ValidateChallenge(def1.Validator.GetChallengeBytes()));
+            Assert.IsTrue(!def1.Validator.ValidateAnswer(def2.Validator.GetAnswerBytes()));
+            Assert.IsTrue(!def2.Validator.ValidateAnswer(def1.Validator.GetAnswerBytes()));
+        }
+
+        
+    }
+}
---
+++ Tests.LightBlueFox.Connect/CustomProtocol/TestMessages.cs
@@ -0,0 +1,42 @@
+﻿using LightBlueFox.Connect.CustomProtocol.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.LightBlueFox.Connect.CustomProtocol
+{
+    internal class TestMessages
+    {
+        [Message]
+        public struct TestMessage1
+        {
+            public double testValue;
+            public string[] TestData;
+
+            public static TaskCompletionSource<TestMessage1>? tcs;
+
+            [MessageHandler]
+            public static void HandleTestMessage1(TestMessage1 testMessage1, MessageInfo i)
+            {
+                if (tcs != null) tcs.SetResult(testMessage1);
+            }
+        }
+
+        [Message]
+        public struct TestMessage2
+        {
+            public float testValue;
+            public int[] TestData;
+
+            public static TaskCompletionSource<TestMessage2>? tcs;
+
+            [MessageHandler]
+            public static void HandleTestMessage2(TestMessage2 testMessage2, MessageInfo i)
+            {
+                if(tcs != null) tcs.SetResult(testMessage2);
+            }
+        }
+    }
+}
